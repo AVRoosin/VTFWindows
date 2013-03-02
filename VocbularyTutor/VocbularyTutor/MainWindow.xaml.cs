c85d3ea76fc4a3455f1004817bea42f408bc348f
AVRoosin <avroosin@mail.ru>
2013-03-02 23:05:09 +0600

    Теперь при выборе пункта меню меняется не только фон но и цвет шрифта. Не придумал как это реализовать на XAML. поэтому пришлось прописывать в коде на С#. Наговнокдил, но не придумал как обработать это событие не тупым епребором ифов. кто помнит рихтера лучше меня - плиз, хелп)))

--- VocbularyTutor/VocbularyTutor/MainWindow.xaml.cs
+++ VocbularyTutor/VocbularyTutor/MainWindow.xaml.cs
@@ -50,5 +50,59 @@ namespace VocbularyTutor
             SettingsWindow sw = new SettingsWindow();
             sw.ShowDialog();
         }
+
+        private void _tabItem1_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _tabItem_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Color ActiveColor = new Color();
+            ActiveColor.A = 255;
+            ActiveColor.R = 161;
+            ActiveColor.G = 255;
+            ActiveColor.B = 6;
+            Color PassiveColor = new Color();
+            PassiveColor.A = 255;
+            PassiveColor.R = 88;
+            PassiveColor.G = 105;
+            PassiveColor.B = 113;
+            if (this._tabItem1.IsSelected==true)
+            {
+                this._tabItem1.Foreground = new SolidColorBrush(PassiveColor);
+            }
+            else
+            {
+                this._tabItem1.Foreground = new SolidColorBrush(ActiveColor);
+            }
+
+            if (this._tabItem2.IsSelected == true)
+            {
+                this._tabItem2.Foreground = new SolidColorBrush(PassiveColor);
+            }
+            else
+            {
+                this._tabItem2.Foreground = new SolidColorBrush(ActiveColor);
+            }
+
+            if (this._tabItem3.IsSelected == true)
+            {
+                this._tabItem3.Foreground = new SolidColorBrush(PassiveColor);
+            }
+            else
+            {
+                this._tabItem3.Foreground = new SolidColorBrush(ActiveColor);
+            }
+
+            if (this._tabItem4.IsSelected == true)
+            {
+                this._tabItem4.Foreground = new SolidColorBrush(PassiveColor);
+            }
+            else
+            {
+                this._tabItem4.Foreground = new SolidColorBrush(ActiveColor);
+            }
+        }
     }
 }
