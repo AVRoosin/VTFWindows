0f45a0a53e135ceddf3e4ba2dc03d4336e6edd69
AVRoosin <avroosin@mail.ru>
2013-03-06 18:26:22 +0600

    Исправил баг. При клике правой мышкой вылетало приложение.

--- VocbularyTutor/VocbularyTutor/MainWindow.xaml.cs
+++ VocbularyTutor/VocbularyTutor/MainWindow.xaml.cs
@@ -19,25 +19,35 @@ namespace VocbularyTutor
     /// </summary>
     public partial class MainWindow : Window
     {
+        Color PassiveColor;
+        Color ActiveColor;
+
+        private void Window_Load(object sender, RoutedEventArgs e)
+        {
+            this._tabItem1.Focus();
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            PassiveColor = new Color();
+            PassiveColor.A = 255;
+            PassiveColor.R = 161;
+            PassiveColor.G = 255;
+            PassiveColor.B = 6;
+            ActiveColor = new Color();
+            ActiveColor.A = 255;
+            ActiveColor.R = 88;
+            ActiveColor.G = 105;
+            ActiveColor.B = 113;
+
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
-
-        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
-        }
-        private void TopTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
-        }
-        private void _menuTabControl_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Window_Drag(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
@@ -50,59 +60,17 @@ namespace VocbularyTutor
             SettingsWindow sw = new SettingsWindow();
             sw.ShowDialog();
         }
-
-        private void _tabItem1_GotFocus_1(object sender, RoutedEventArgs e)
+        private void _tabItem_LostFocus(object sender, RoutedEventArgs e)
         {
-
+            TabItem tab = new TabItem();
+            tab = (TabItem)sender;
+            tab.Foreground = new SolidColorBrush(PassiveColor);
         }
-
         private void _tabItem_GotFocus(object sender, RoutedEventArgs e)
         {
-            Color ActiveColor = new Color();
-            ActiveColor.A = 255;
-            ActiveColor.R = 161;
-            ActiveColor.G = 255;
-            ActiveColor.B = 6;
-            Color PassiveColor = new Color();
-            PassiveColor.A = 255;
-            PassiveColor.R = 88;
-            PassiveColor.G = 105;
-            PassiveColor.B = 113;
-            if (this._tabItem1.IsSelected==true)
-            {
-                this._tabItem1.Foreground = new SolidColorBrush(PassiveColor);
-            }
-            else
-            {
-                this._tabItem1.Foreground = new SolidColorBrush(ActiveColor);
-            }
-
-            if (this._tabItem2.IsSelected == true)
-            {
-                this._tabItem2.Foreground = new SolidColorBrush(PassiveColor);
-            }
-            else
-            {
-                this._tabItem2.Foreground = new SolidColorBrush(ActiveColor);
-            }
-
-            if (this._tabItem3.IsSelected == true)
-            {
-                this._tabItem3.Foreground = new SolidColorBrush(PassiveColor);
-            }
-            else
-            {
-                this._tabItem3.Foreground = new SolidColorBrush(ActiveColor);
-            }
-
-            if (this._tabItem4.IsSelected == true)
-            {
-                this._tabItem4.Foreground = new SolidColorBrush(PassiveColor);
-            }
-            else
-            {
-                this._tabItem4.Foreground = new SolidColorBrush(ActiveColor);
-            }
+            TabItem tab = new TabItem();
+            tab = (TabItem)sender;
+            tab.Foreground = new SolidColorBrush(ActiveColor);
         }
     }
 }
