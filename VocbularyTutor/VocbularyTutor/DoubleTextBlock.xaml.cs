3a7ec454d3690ba8e64cda54fa20b3aa237aaeb6
AVRoosin <avroosin@mail.ru>
2013-03-09 20:33:47 +0600

    Добавил новый элемент управления - двойной текстблок - чтобы был сразу перевод и комментарий к нему

---
+++ VocbularyTutor/VocbularyTutor/DoubleTextBlock.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VocbularyTutor
+{
+	/// <summary>
+	/// Interaction logic for DoubleTextBlock.xaml
+	/// </summary>
+    public partial class DoubleTextBlock : UserControl
+    {
+        public string MainText
+        {get { return (string)GetValue(MainTextProperty); }
+            set { SetValue(MainTextProperty, value); } }
+        public static readonly DependencyProperty MainTextProperty = DependencyProperty.Register("MainText", typeof(string), typeof(DoubleTextBlock), new UIPropertyMetadata(""));
+        public string CommentText
+        {
+            get { return (string)GetValue(CommentTextProperty); }
+            set { SetValue(CommentTextProperty, value); }
+        }
+        public static readonly DependencyProperty CommentTextProperty = DependencyProperty.Register("CommentText", typeof(string), typeof(DoubleTextBlock), new UIPropertyMetadata(""));
+        public DoubleTextBlock()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
